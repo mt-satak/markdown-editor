68b799917f206f30c786d22e69a52c9f08b27f05
mt-satak <sakiware@gmail.com>
2021-08-15 17:51:39 +0900

    履歴画面とのルーティング設定を追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
 import { render } from 'react-dom'
-import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import { Editor } from './pages/editor'
+import { History } from './pages/history'
 
 const GlobalStyle = createGlobalStyle`
   body * {
@@ -10,11 +16,22 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-  const Main = (
-    <>
-      <GlobalStyle />
-      <Editor />
-    </>
-  )
+const Main = (
+  <>
+    <GlobalStyle />
+    {/* Router: ルーティング範囲を定義 */}
+    <Router>
+      <Route exact={true} path="/editor">
+        <Editor />
+      </Route>
+
+      <Route exact={true} path="/history">
+        <History />
+      </Route>
+      {/* 定義されていないパスの場合は/editorにリダイレクトする */}
+      <Redirect to="/editor" path="*" />
+    </Router>
+  </>
+)
 
 render(Main, document.getElementById('app'))
